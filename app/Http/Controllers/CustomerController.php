63e8bf7f291a97515a4c38382f751eb28161f0a0
khalidullayevad <57382444+khalidullayevad@users.noreply.github.com>
2025-10-16 20:59:17 +0500

    customer crud

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,47 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $customers = Customer::latest()->paginate(5);
+        return view('customers.index', compact('customers'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    public function create()
+    {
+        return view('customers.create');
+    }
+
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required'
+        ]);
+        Customer::create($request->all());
+
+        return redirect()->route('customers.index')
+            ->with('success', 'Customer created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Customer $customer)
     {
-        //
+        return view('customers.show', compact('customer'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function edit(Customer $customer)
     {
-        //
+        return view('customers.edit', compact('customer'));
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function update(Request $request, Customer $customer)
     {
-        //
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required'
+
+        ]);
+
+        $customer->update($request->all());
+        return redirect()->route('customers.index')
+            ->with('success', 'Customer updated successfully');
+    }
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return redirect()->route('customers.index')
+            ->with('success', 'Customer deleted successfully');
     }
 }
--- resources/views/customers.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layauts.app')
-
-@section('content')
-    <h2>H</h2>
-@endsection
